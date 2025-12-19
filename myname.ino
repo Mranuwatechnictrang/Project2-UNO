a66c2c87bab7809e1b5b64300964d376715f2d25
Mranuwatechnictrang <138744220+Mranuwatechnictrang@users.noreply.github.com>
2025-12-19 10:46:16 +0700

    Add files via upload

---
+++ myname.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  Serial.println("Mr.anuwat Sucharto");
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
